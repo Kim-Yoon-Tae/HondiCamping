089cb2f84fb904a08401b001b4e6b69aed96dcab
eve-Lover <nayy6947@gmail.com>
2022-06-12 05:35:06 +0900

    고캠핑 api 요청 코드 수정, 요청 느림 현상 개선 완료

--- hondi_ny/lib/HomeUI.dart
+++ hondi_ny/lib/HomeUI.dart
@@ -12,70 +12,75 @@ class HomeUI extends StatefulWidget {
 
 class _HomeUIState extends State<HomeUI> {
 
+  var data;
 
-  final authkey = 'q6tw/AWLdEFNCHf6qcCrXFWErhIdK4fr9cHdlIX/2KRVOvi90Jr3f8u3/SvhBH4mTcgzOu5I6nRlKvWwem2WKw=='; // 나영 인증키
-  var json;
-  var data = {};
-  var mapx = 126.8395857; // (현재 GPS좌표)(임시로 표선으로 저장)
-  var mapy = 33.3259761; // (현재 GPS좌표)(임시로 표선으로 저장)
+  int len = 0;
 
-  getData() async { // ${mapX}&mapY=${mapY}
-    var response
+  final String authkey = 'q6tw/AWLdEFNCHf6qcCrXFWErhIdK4fr9cHdlIX/2KRVOvi90Jr3f8u3/SvhBH4mTcgzOu5I6nRlKvWwem2WKw=='; // 나영 인증키
+  double mapx = 128.6142847;// 126.8395857;//  // (현재 GPS좌표)(샘플데이터 기준)
+  double mapy = 36.0345423;  // 33.3259761;// (현재 GPS좌표)(샘플데이터 기준)
+
+  getData() async {
+    final http.Response response
     = await http.get(Uri.parse
       ('http://api.visitkorea.or.kr/openapi/service/rest/GoCamping/locationBasedList?'
         'ServiceKey=${authkey}'
         '&MobileOS=ETC&MobileApp=AppTest&mapX=${mapx}&mapY=${mapy}&radius=2000&_type=json'));
-    var body = utf8.decode(response.bodyBytes);
 
     if (response.statusCode == 200) {
-      print(body); // 확인용
-      json = jsonDecode(body);
 
-      if (json['response']['header']['resultCode'] == "0000") {
-        if (json['response']['body']['items'] == '') {
-          showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(
-                  content: Text("마지막 데이터 입니다."),
-                );
-              });
-        } else {
-          print(json); //확인용
+      var body = utf8.decode(response.bodyBytes);
+      //print("body \n $body"); // 확인용
+
+      var json = jsonDecode(body);
+      //print("json \n $json"); // 확인용
+      setState((){
+        data = json['response']['body']['items']['item'];
+        if(data is List){ // totalCount가 2개 이상일 때는 list로 받게 됨.
+          len = data.length;
+        }else if(data is Map){ // totalCount가 하나일 때는 map으로 받게 됨.
+          len = 1;
         }
-      } else {
-        // 만약 응답이 OK가 아니면, 에러를 던집니다.
-        throw Exception('Failed to load post');
-      }
-    }
+      });
 
-     data = json['response']['body']['items'];
-      print(data);
-     print(data.length); //확인용
+      print("data \n $data"); // 확인용
+      //print(data.runtimeType); // 확인용
+
+    } else {
+      // 만약 응답이 OK가 아니면, 에러를 던집니다.
+      throw Exception('Failed to load post');
+    }
   }
 
 
-    @override
+  @override
   void initState() { // 위젯 초기화시 1번 호출
     super.initState();
     getData();
   }
+
+
   @override
   Widget build(BuildContext context) {
-
-    if (data.isNotEmpty){
+    if (data != null) {
       return Scaffold(
           body: Container(
-           child: ListView.builder(
-                itemCount: data.length,
-                itemBuilder: (c,i){
-                  return Text(data["item"]["facltNm"]?? 'default value'); // 야영장명 출력
-                }),
+            child: ListView.builder(
+                itemCount: len,
+                itemBuilder: (c, i) {
+                  if(data is List) {
+                    return Text(
+                        data[i]["facltNm"] ?? 'default value');
+                  }else{
+                    return Text(
+                        data["facltNm"].toString());// 야영장명 출력
+                  }
+                }
+            ),
           )
       );
     } else { // 로딩중 이미지 출력
-      return  CircularProgressIndicator();
+      return CircularProgressIndicator();
     }
-
   }
 }
